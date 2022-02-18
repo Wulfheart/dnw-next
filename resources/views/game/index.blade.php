8e22b47cfdb8cd1d4281e5a60e8b8c2a9bbdc0ca
Alexander Wulf <alex.f.wulf@gmail.com>
2022-02-18 22:34:29 +0100

    WIP

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -7,12 +7,19 @@
             </div>
         </div>
         <?php /** @var \App\Models\Game $game */ ?>
-        <div class="grid grid-cols-3 gap-10">
+        <div class="grid grid-cols-4 gap-10">
         @foreach($preview['new'] as $game)
-            <div class="h-full w-full rounded-xl bg-black/60 bg-blend-darken bg-center bg-cover p-5"
+            <div class="h-full w-full rounded-xl bg-black/60 bg-blend-darken bg-center bg-cover p-5 text-white"
                 style="background-image: url('{{storage_asset($game->currentPhase->svg_adjudicated)}}')"
             >
-                <h3 class="text-white font-bold text-xl line-clamp-2">{{$game->name}}</h3>
+                <div class="h-full flex flex-col justify-between">
+                <h3 class="font-bold text-xl line-clamp-2 mb-4">{{$game->name}}</h3>
+                <div class="flex flex-row justify-between space-x-5">
+                    <div class="font-medium">5/7 Spieler</div>
+                    <div class="font-medium">12 h</div>
+                </div>
+
+                </div>
 
             </div>
         @endforeach
