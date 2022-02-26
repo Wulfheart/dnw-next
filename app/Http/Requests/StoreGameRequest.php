1a14287a5c031864a63fc87694c4a04357dce744
Alexander Wulf <alex.f.wulf@gmail.com>
2022-02-26 20:34:15 +0100

    Correct no adjudication days

--- app/Http/Requests/StoreGameRequest.php
+++ app/Http/Requests/StoreGameRequest.php
@@ -27,8 +27,14 @@ class StoreGameRequest extends FormRequest
             'name' => ['required', 'string', 'unique:games'],
             'variant_id' => ['required', 'integer', 'exists:variants,id'],
             'phase_length' => ['integer', 'gte:5'],
-            'no_adjudication' => ['array'],
-            'no_adjudication.*' => ['boolean', 'accepted'],
+            'no_adjudication' => ['array', function($attribute, $value, $fail){
+                $sum = collect($value)->values()->sum();
+                ray($sum);
+                if($sum > 6){
+                    $fail("Cannot pause on all seven days of the week");
+                }
+            }],
+            'no_adjudication.*' => ['boolean'],
             // 'start_when_ready' => ['boolean'],
             // 'join_phase_length' => ['required', 'gte:5', 'integer']
         ];
--- tests/Feature/Actions/Game/CreateGameActionTest.php
+++ tests/Feature/Actions/Game/CreateGameActionTest.php
@@ -25,6 +25,7 @@ it('can create a game without "no adjudication"', function () {
         'phase_length' => CarbonInterface::HOURS_PER_DAY * CarbonInterface::MINUTES_PER_HOUR,
     ]);
 
+    $response->assertSessionHasNoErrors();
     $response->assertRedirect();
 
     expect(Game::count())->toBeOne();
@@ -44,10 +45,49 @@ it('can create a game with "no adjudication day"', function () {
         'name' => 'Conquerors of the old world',
         'variant_id' => Variant::first()->id,
         'phase_length' => CarbonInterface::HOURS_PER_DAY * CarbonInterface::MINUTES_PER_HOUR,
-        // 'no_adjudication' => [1,2]
+        'no_adjudication' => [
+            1 => 1,
+            2 => 1,
+            3 => 1,
+            4 => 1,
+            5 => 1,
+            6 => 1,
+            7 => 0,
+        ],
+    ]);
+    $response->assertSessionHasNoErrors();
+
+    $response->assertRedirect();
+
+    expect(Game::count())->toEqual(1);
+    /** @var Game $game */
+    $game = Game::with('noAdjudicationDays')->first();
+    expect($game->noAdjudicationDays()->count())->toEqual(6);
+});
+
+it('cannot create a game with seven "no adjudication day"', function () {
+    Queue::fake();
+    $this->seed(VariantSeeder::class);
+
+    $user = User::factory()->create();
+    actingAs($user);
+
+
+    $response = $this->post(route('games.store'), [
+        'name' => 'Conquerors of the old world',
+        'variant_id' => Variant::first()->id,
+        'phase_length' => CarbonInterface::HOURS_PER_DAY * CarbonInterface::MINUTES_PER_HOUR,
+        'no_adjudication' => [
+            1 => 1,
+            2 => 1,
+            3 => 1,
+            4 => 1,
+            5 => 1,
+            6 => 1,
+            7 => 1,
+        ],
     ]);
 
-    // $response->assertRedirect();
-    expect(Game::count())->toBeOne();
-    // InitializeGameAction::assertPushed();
-})->skip(true, "TODO");
+    $response->assertSessionHasErrors('no_adjudication');
+    expect(Game::count())->toEqual(0);
+});
