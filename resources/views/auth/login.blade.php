f5c0363a740a088bc2d68fede2491af5d6fb76a5
Alexander Wulf <alex.f.wulf@gmail.com>
2022-07-21 00:02:47 +0200

    Dev login added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,19 +12,23 @@
             </div>
         @endif
 
+        @env('local')
+            <x-login-link />
+        @endenv
+
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
             <div>
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                    required autofocus />
+                             required autofocus />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                    autocomplete="current-password" />
+                             autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -37,7 +41,7 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                        href="{{ route('password.request') }}">
+                       href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
