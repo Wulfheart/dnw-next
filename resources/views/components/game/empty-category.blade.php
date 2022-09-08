fee0013d9deaf321524e76ff48a092bc89c622b9
Alexander Wulf <aw@dc.ag>
2022-09-08 21:43:09 +0200

    Sad face

--- resources/views/components/game/empty-category.blade.php
+++ resources/views/components/game/empty-category.blade.php
@@ -1,7 +1,7 @@
 <div class="h-full w-full col-span-4">
     <span
         class="relative block w-full h-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center flex flex-col justify-center">
-        <x-heroicon-o-emoji-sad class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" />
+        <x-heroicon-o-face-frown class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" />
         <span class="mt-2 block text-sm font-medium text-gray-900"> Keine Spiele gefunden </span>
     </span>
 </div>
