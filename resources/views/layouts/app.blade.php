e9010ff6bd52c46a477ed3abbf87ef63fe3cf2d0
Alexander Wulf <aw@dc.ag>
2022-09-09 15:32:41 +0200

    Clarity script added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,5 +47,13 @@
     <script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/toolbar.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/metrics.js"></script>
 @endif
+
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "dkxorxd9xl");
+</script>
 </body>
 </html>
